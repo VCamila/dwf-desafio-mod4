31a977d093e5d09903a2fcd15984b3cbb953d37a
Camila Vazquez <camilavazquezvazquez2014@outlook.com>
2022-03-03 19:27:06 -0300

    nuevas modificaciones

--- componentes/portfolio.js
+++ componentes/portfolio.js
@@ -10,12 +10,11 @@ newComponentePortfolio.innerHTML =
 <div class="portfolio-content"> 
 <div class="port-cards"> 
 <img src="https://escucharahoraysiempre.com/wp-content/uploads/2019/03/implante-coclear1.png" class="portfolio-imagen" alt=""/>     
-<h4 class="portfolio-title"> Oír es un derecho</h4>
+   <h4 class="portfolio-title"> Oír es un derecho</h4>
     <p class="portfolio-text"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto temporibus voluptatum reprehenderit, nisi numquam ab. Incidunt mollitia aut nam perferendis rem doloremque asperiores dolor, architecto accusamus, commodi consequatur eligendi voluptate?</p>
     <a href="https://apx.school/" class="portfolio-link"> Ver más</a>
     </div>
   </div>
-</div>  
 </template>
 `
 
